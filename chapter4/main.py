bea3ac72ca576ec048834de0b60896d67324a942
byeongcheollim <lim6112j@gmail.com>
2025-02-14 02:35:10 +0900

    ..

--- chapter4/main.py
+++ chapter4/main.py
@@ -10,4 +10,20 @@ cafe = bytes('café', encoding='utf_8')
 cafe[0]
 cafe_arr = bytearray(cafe)
 cafe_arr[-1:]
-
+import sys, locale
+expressions = """
+locale.getpreferredencoding()
+type(my_file)
+sys.stdout.isatty()
+sys.stdout.encoding
+sys.stdin.isatty()
+sys.stdin.encoding
+sys.stderr.isatty()
+sys.stderr.encoding
+sys.getdefaultencoding()
+sys.getfilesystemencoding()
+"""
+my_file = open('dummy', 'w')
+for expression in expressions.split():
+    value = eval(expression)
+    print(expression.rjust(30), '->', repr(value))
