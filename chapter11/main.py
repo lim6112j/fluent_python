302f7dda483cd90cb3953cd3e39b7fb5c3fd2be8
byeongcheollim <lim6112j@gmail.com>
2025-05-20 00:35:19 +0900

    chapter11

--- chapter11/main.py
+++ chapter11/main.py
@@ -1,8 +1,14 @@
 import collections
-
+import math
 
 class Vector2d:
     typecode = 'd'
+
+    @classmethod
+    def fromBytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
 
     def __init__(self, x, y):
         self.__x = float(x)
@@ -19,6 +25,46 @@ class Vector2d:
     def __iter__(self):
         return (i for i in (self.x, self.y))
 
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{}({!r},{!r})'.format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) +
+                bytes(array(self.typecode, self)))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('p'):
+            fmt_spec = fmt_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
+
+vec = Vector2d(4, 3)
+print ("{}\n", vec)
+print ("{}\n", tuple(vec))
+print ("{}\n", vec.angle())
+print (format(vec, '0.5fp'))
 
 class Foo:
     def __getitem__(self, pos):
